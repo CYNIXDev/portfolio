44321c3f58fac96a0aa8335207f648215ce38332
Nopparit Salasri <nprxart@hotmail.com>
2024-04-11 17:57:04 +0700

    Integate all component in one file on index.js and config vite component path

---
+++ src/component/index.js
@@ -0,0 +1,9 @@
+export { default as About } from "./About";
+export { default as Header } from "./Header";
+export { default as Title } from "./Title";
+export { default as Hero } from "./Hero";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as Contact } from "./Contact";
+export { default as CircleBackground } from "./CircleBackground";
+export { default as Footer } from "./Footer";
+export { default as NavSocial } from "./NavSocial";
